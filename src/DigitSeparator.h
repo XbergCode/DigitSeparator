47d9a068665b761b9d3d65d251c0469f54aec4f9
XbergCode <105010135+XbergCode@users.noreply.github.com>
2023-09-02 22:26:08 +0000

    Update DigitSeparator.h
    Fixed values that starts with 0 did not show, Integer in digitSeparateD needed "fabs()".
    Added default Thousand & Decimal mark's to the library.

--- src/DigitSeparator.h
+++ src/DigitSeparator.h
@@ -2,17 +2,22 @@
 
 //    FILE: DigitSeparator.h
 //  AUTHOR: XbergCode
-// VERSION: 0.1.1
+// VERSION: 0.1.2
 // PURPOSE: Digit Separator library for Arduino. Separates digit value with marks and returns it as C string.
 //     URL: https://github.com/XbergCode/DigitSeparator
 
 
-#define DigitSeparator_VERSION    (F("0.1.1\n"))                      // Verison 
+#define DigitSeparator_VERSION    (F("0.1.2\n"))                      // Verison 
 
 #include <Arduino.h>                                                  // Include The Arduino Library
 
-char * digitSeparate(int64_t, char);                                  // Digit Separator - Signed & Unsigned
-char * digitSeparateU(uint64_t, char);                                // Digit Separator - Unsigned
-char * digitSeparateD(const double, const uint8_t, char, char);       // Digit Separator - Double & Float
+// Default Separator Mark's
+const char defaultThousand = '.';                                     // Default Thousand Mark
+const char defaultDecimal  = ',';                                     // Default Decimal Mark
+
+// Digit Separator's
+char * digitSeparate(int64_t, char tMARK = defaultThousand);          // Digit Separator - Signed & Unsigned
+char * digitSeparateU(uint64_t, char tMARK = defaultThousand);        // Digit Separator - Unsigned
+char * digitSeparateD(const double, const uint8_t Dec = 2U, char tMARK = defaultThousand, char dMARK = defaultDecimal); // Digit Separator - Double & Float
 
 // End Of File.
